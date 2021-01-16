0e3c0b9d39f554bf709eb016e00f41fbca242ef2
Craig Schulstad <CraigASchulstad@gmail.com>
2021-01-16 17:00:35 -0600

    Update loader.c
    The MUI enhancement has been reapplied to version 6.0 of the file, although there really has been no actual change to this file since version 5.22.

--- loader.c
+++ loader.c
@@ -23,14 +23,14 @@
 /*******************************************************************************/
 /* Modification and Enhancement Narrative                                      */
 /*                                                                             */
-/* Craig Schulstad - Horace, ND  USA (7 December, 2020)                        */
+/* Craig Schulstad - Horace, ND  USA (16 January, 2021)                        */
 /*                                                                             */
 /* This program has been revised to reactively acquire an MUI file reference   */
 /* to be used by the various resource fetch functions.  Without these code     */
 /* changes, no MUI reference was found and the calling program was falling     */
-/* back to the exe file for information.                                       */
+/* back to the "exe" file for information.                                     */
 /*                                                                             */
-/* Version being enhanced:   6.0-rc1                                           */
+/* Version being enhanced:  6.0                                                */
 /*                                                                             */
 /* The following function calls were added:                                    */
 /*   get_mui (Attempts to locate and retrieve an MUI file)                     */
