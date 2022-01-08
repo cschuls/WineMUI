7b11a820aeb3a55eb4fb068588d92ec69fc6e811
Craig Schulstad <CraigASchulstad@gmail.com>
2022-01-08 12:18:23 -0600

    Update loader.c
    I reapplied the multi-language resource enhancement to this version of the "loader.c" file.

--- loader.c
+++ loader.c
@@ -23,14 +23,14 @@
 /*******************************************************************************/
 /* Modification and Enhancement Narrative                                      */
 /*                                                                             */
-/* Craig Schulstad - Horace, ND  USA (3 January, 2022)                         */
+/* Craig Schulstad - Horace, ND  USA (8 January, 2022)                         */
 /*                                                                             */
 /* This program has been revised to reactively acquire an MUI file reference   */
 /* to be used by the various resource fetch functions.  Without these code     */
 /* changes, no MUI reference was found and the calling program was falling     */
 /* back to the "exe" file for information.                                     */
 /*                                                                             */
-/* Version being enhanced:  7.0-rc4                                            */
+/* Version being enhanced:  7.0-rc5                                            */
 /*                                                                             */
 /* The following function calls were added:                                    */
 /*   get_mui (Attempts to locate and retrieve an MUI file)                     */
