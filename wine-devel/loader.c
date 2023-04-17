f3234fc05c430473894f2c42a978f6908e81584b
Craig Schulstad <CraigASchulstad@gmail.com>
2023-04-17 12:40:15 -0500

    Update loader.c
    Reapplied the multi-language resource enhancement to version 8.6

--- wine-devel/loader.c
+++ wine-devel/loader.c
@@ -23,14 +23,14 @@
 /*******************************************************************************/
 /* Modification and Enhancement Narrative                                      */
 /*                                                                             */
-/* Craig Schulstad - Horace, ND  USA (31 March, 2023)                          */
+/* Craig Schulstad - Horace, ND  USA (16 April, 2023)                          */
 /*                                                                             */
 /* This program has been revised to reactively acquire an MUI file reference   */
 /* to be used by the various resource fetch functions.  Without these code     */
 /* changes, no MUI reference was found and the calling program was falling     */
 /* back to the "exe" file for information.                                     */
 /*                                                                             */
-/* Version being enhanced:  8.5                                                */
+/* Version being enhanced:  8.6                                                */
 /*                                                                             */
 /* The following function calls were added:                                    */
 /*   get_mui (Attempts to locate and retrieve an MUI file)                     */
