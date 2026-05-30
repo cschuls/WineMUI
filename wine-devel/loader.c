8dcb08bd827200c7b618370a93c2886f97768c1f
Craig Schulstad <CraigASchulstad@gmail.com>
2026-05-30 09:29:58 -0500

    Update loader.c
    Reapplied the language specific resource access enhancements

--- wine-devel/loader.c
+++ wine-devel/loader.c
@@ -23,14 +23,14 @@
 /*******************************************************************************/
 /* Modification and Enhancement Narrative                                      */
 /*                                                                             */
-/* Craig Schulstad - Horace, ND  USA (16 May, 2026)                            */
+/* Craig Schulstad - Horace, ND  USA (30 May, 2026)                            */
 /*                                                                             */
 /* This program has been revised to reactively acquire an MUI file reference   */
 /* to be used by the various resource fetch functions.  Without these code     */
 /* changes, no MUI reference was found and the calling program was falling     */
 /* back to the "exe" file for information.                                     */
 /*                                                                             */
-/* Version being enhanced:  11.9                                               */
+/* Version being enhanced:  11.10                                              */
 /*                                                                             */
 /* The following function calls were added:                                    */
 /*   get_mui (Attempts to locate and retrieve an MUI file)                     */
@@ -1092,7 +1092,6 @@ BOOL WINAPI DECLSPEC_HOTPATCH EnumResourceTypesExW( HMODULE module, ENUMRESTYPEP
     return ret;
 }
 
-
 /* MUI Start */
 /***********************************************************************/
 /* get_mui - Acquire an MUI module for the associated resource         */
@@ -1250,7 +1249,6 @@ BOOL WINAPI DECLSPEC_HOTPATCH FreeResource( HGLOBAL handle )
 {
     return FALSE;
 }
-
 
 /* MUI Start */
 /**********************************************************************
